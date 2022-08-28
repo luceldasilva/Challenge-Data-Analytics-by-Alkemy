66c94d0e116458ff96d183b76058138b89e86a39
Lucel Da Silva <luceldasilva@yahoo.com>
2022-08-27 23:12:37 -0400

    actualizando conexion a base de datos y lector de archivos

--- conexion_db.py
+++ conexion_db.py
@@ -1,8 +1,8 @@
 import logging
 import glob
-# from decouple import config
-# from sqlalchemy import create_engine as ce
-import pandas as pd
+from decouple import config
+from sqlalchemy import create_engine as ce
+# import pandas as pd
 
 logging.basicConfig(
     format = '%(asctime)-5s %(name)-15s %(levelname)-8s %(message)s', 
@@ -11,35 +11,39 @@ logging.basicConfig(
     encoding="utf-8"
     )
 
+PATH_TO_FILE = './/sql_scripts//table_create.sql'
 
 files = glob.glob('.//**//**//' + '*.csv')
-# print(files)
-for f in files:
-    print(f)
-    data = pd.read_csv(f,
-                       header = 0,
-                       sep = ',')
-    # print(data.to_markdown())
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-        print(data)
+# # print(files)
+# for f in files:
+#     print(f)
+#     data = pd.read_csv(f,
+#                        header = 0,
+#                        sep = ',')
+#     # print(data.to_markdown())
+#     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+#         print(data)
 
-# try:
-#     pg_engine = ce(config('engine_psql'))
-#     logging.info("Conexión exitosa.")
-#     connection = pg_engine.raw_connection()
-#     cursor = connection.cursor()
-#     cursor.execute("SELECT version()")
-#     row = cursor.fetchone()
-#     logging.info("Versión del servidor de PostgreSQL: {}".format(row))
-#     # cursor.execute("SELECT * FROM ago22")
-#     # rows = cursor.fetchall()
-#     # for row in rows:
-#     #     print(row)
+try:
+    pg_engine = ce(config('engine_psql'))
+    logging.info("Conexión exitosa.")
+    connection = pg_engine.raw_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT version()")
+    row = cursor.fetchone()
+    logging.info("Versión del servidor de PostgreSQL: {}".format(row))
+    with open(PATH_TO_FILE, 'r', encoding='utf-8') as myfile:
+        data = myfile.read()
+        cursor.execute(data)
+    cursor.execute("SELECT * FROM alkemy")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-# except Exception as ex:
-#     logging.error("Error durante la conexión: {}".format(ex))
+except Exception as ex:
+    logging.error("Error durante la conexión: {}".format(ex))
 
-# finally:
-#     pg_engine.dispose()
-#     logging.info("La conexión ha finalizado.")
+finally:
+    pg_engine.dispose()
+    logging.info("La conexión ha finalizado.")
     
