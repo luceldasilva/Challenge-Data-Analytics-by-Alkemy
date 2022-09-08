a332b8e87efdc82ffbf07702373660c77ebfb12d
Lucel Da Silva <luceldasilva@yahoo.com>
2022-09-07 21:56:19 -0400

    creacion de readme, archivos txt para instrucciones de instalacion y unificar los py a alkemy.py

---
+++ README.md
@@ -0,0 +1,74 @@
+# Challenge Data Analytics - Python
+
+> Este es mi repositorio del challenge de Alkemy
+
+## Objetivo 👈
+Crear un proyecto que consuma datos desde 3 fuentes distintas para tener una base de datos SQL con información cultural sobre bibliotecas, museos y salas de cines argentinos.
+
+## Estructura del Proyecto 📦
+
+    ├── data           <- Carpeta raíz que almacene las descargas.
+	│   ├── biblioteca
+	│   │    ├── Mes de descarga
+	│   │    │     └── biblioteca-día-de-la-descarga.csv
+    │   ├── cine
+	│   │    ├── Mes de descarga
+	│   │    │     └── cine-día-de-la-descarga.csv
+    │   ├── museos
+	│   │    ├── Mes de descarga
+	│   │    │     └── museos-día-de-la-descarga.csv
+	│
+	├── venv           <- Carpeta del ambiente virtual
+    ├── .env     <- Archivo para la conexión de base de datos y descargas.
+	├── .gitignore     <- Ignorar carpetas data que el programa creará y venv.
+	├── alkemy.log             <- Archivo de registro que sucede en el código.
+    ├── alkemy.py             <- La estrella del proyecto.
+    ├── puntoenv.txt          <- Plantilla para crear el archivo .env
+	├── README.md          <- La guía del proyecto.
+    ├── requirements.txt            <- Librerías del proyecto
+    ├── table_create.sql   <- Tabla inicial para manipular los datos en sql
+
+## Proceso de Instalación ⚙️
+![](https://thumbs.gfycat.com/UglyEminentEidolonhelvum-size_restricted.gif)
+
+1. Clonar repositorio
+``` bash
+git clone https://github.com/luceldasilva/cookiecutter-personal.git
+```
+también se puede [descargar como archivo.zip aquí](https://github.com/luceldasilva/cookiecutter-personal/archive/refs/heads/main.zip "descargar en archivo.zip aquí") y descomprimirlo.
+
+2. Crear ambiente virtual para ejecutar el programa
+En Windows
+``` bash
+py -m venv venv
+venv\Scripts\activate
+```
+En Linux y Mac
+``` bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+3. Instalar las librerías contenidas en requirements.txt
+``` bash
+pip install -r requirements.txt
+```
+
+4. Crear archivo .env copiando del archivo puntoenv.txt
+> Reemplazar por el password correspondiente de su usuario
+![Imgur](https://i.imgur.com/4jExZDY.png)
+
+Todo Listo ya se podrá ejecutar el alkemy.py en tu directorio
+En Windows
+``` bash
+py alkemy.py
+```
+En Linux y Mac
+``` bash
+python3 alkemy.py
+```
+
+## License 🧾
+The MIT License (MIT)
+
+![](https://i.pinimg.com/originals/a4/9d/89/a49d89969bd34bb144e6fb9664d825a1.gif)
--- alkemy.py
+++ alkemy.py
@@ -1,11 +1,24 @@
+# Importando librerías
 import logging
-from os.path import abspath
-from pathlib import Path
-import pyprojroot
 from decouple import config
+from datetime import datetime
+import os
+from pathlib import Path
+from os.path import isdir
+from os.path import abspath
+from shutil import rmtree
+import pyprojroot
+import pyhere
+import requests as req
 from sqlalchemy import create_engine as ce
 import numpy as np
 import pandas as pd
+
+
+now = datetime.now()
+mes = now.strftime('%Y-%B')
+today = now.strftime('%d-%m-%Y')
+
 
 logging.basicConfig(
     format = '%(asctime)-5s %(name)-15s %(levelname)-8s %(message)s', 
@@ -13,6 +26,36 @@ logging.basicConfig(
     filename='alkemy.log',  
     encoding="utf-8"
     )
+
+
+def create_data():
+    """
+    Crear carpeta raíz del proyecto
+    """
+    if not isdir('data'):
+        logging.error('No existe la carpeta ./data/')
+        os.mkdir('data')
+        logging.info('Carpeta creada')
+    else:
+        logging.info('La carpeta está disponible')
+
+
+def downloads(file_link, art_venue):
+    """
+    Descargar los archivos fuentes
+    """
+    logging.info(f'Descargando {art_venue}')
+    data_dir = pyprojroot.here('data')
+    os.chdir(str(data_dir))
+    with req.get(str(file_link)) as rq:
+        if isdir(str(art_venue)):
+            rmtree(str(art_venue))
+        os.makedirs(os.path.join(data_dir, str(art_venue), str(mes)))
+        os.chdir(os.path.join(data_dir, str(art_venue), str(mes)))
+        with open(f'{str(art_venue)}-{str(today)}.csv', 'wb') as file:
+            file.write(rq.content)
+            logging.info(f'categoría {art_venue} descargada')
+
 
 def manipulation_sql():
     """
@@ -26,6 +69,7 @@ def manipulation_sql():
         cursor.execute("SELECT version()")
         row = cursor.fetchone()
         logging.info("Versión del servidor de PostgreSQL: {}".format(row))
+        os.chdir(str(pyhere.here().resolve()))
         with open(str(abspath('table_create.sql')), 'r', encoding='utf-8') as myfile:
             tc = myfile.read()
             cursor.execute(tc)
@@ -93,7 +137,9 @@ def manipulation_sql():
             butter = [str(cat_count_df), str(fuente_count_df), str(province_df)]
             print(".·-=-·." * 11)
             while show_table < len(butter):
-                print(butter[show_table]) #juego de palabras
+                print('Cantidades Totales')
+                print("-" * 11)
+                print(butter[show_table])
                 print(".·-=-·." * 11)
                 show_table += 1 
 
@@ -101,11 +147,41 @@ def manipulation_sql():
         pg_engine.dispose()
         logging.info("La conexión ha finalizado.")
 
-    
+def cine():
+    """
+    Tabla exclusiva de cine para mostrar cantidades de pantallas, butacas y disponibilidad de espacios INCAA
+    """
+    logging.info('Creando tabla de cine')
+    dir_cine = Path(str(pyprojroot.here('data').joinpath('cine'))).glob("**/*.csv")
+    for buscar_cine in dir_cine:
+        df_cine = pd.read_csv(buscar_cine, header = 0, sep = ',')
+        df_cine.columns = df_cine.columns.str.lower()
+        df_cine.espacio_incaa = df_cine.espacio_incaa.replace(['0'], np.nan)
+        df_cine.espacio_incaa = df_cine.espacio_incaa.fillna('No')
+        df_cine.espacio_incaa = df_cine.espacio_incaa.str.lower()
+        df_cine = df_cine.replace(['Ciudad Autónoma de Buenos Aires'], ['Capital Federal'])
+        df_cine.provincia = df_cine.provincia.str.strip()
+        espacios = df_cine.groupby(['provincia'])['espacio_incaa'].apply(lambda x: x[x == 'si'].count()).reset_index(name='espacios INCAA').sort_values('provincia', ascending=1)    
+        cine_table = df_cine.groupby(['provincia']).aggregate({'pantallas':'sum','butacas':'sum'}).sort_values('provincia', ascending=1)
+        cine_table = cine_table.merge(espacios, on='provincia')
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.precision', 3):
+            print('Tabla de Salas de Cine')
+            print("-" * 11)
+            print(cine_table)
+            print(".·-=-·." * 10)
+            logging.info('Tabla de cine impresa')
+
+
 def run():
+    print('Hola, estoy cargando datos')
+    logging.info('Inicio de ejecución del programa')
+    create_data()
+    downloads(config('museo_link'), 'museos')
+    downloads(config('cine_link'), 'cine')
+    downloads(config('biblioteca_link'), 'biblioteca')
     manipulation_sql()
+    cine()
+    logging.info('Fin de la ejecución y cierre del programa')
 
 if __name__ == '__main__':
     run()
-
-    
